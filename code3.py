aca9289392442e24d09a34cc30541b9d6faaad05
banalybe <jurkute.agne@gmail.com>
2021-04-11 15:21:57 +0300

    Adding new files

---
+++ code3.py
@@ -0,0 +1,18 @@
+def multiplication_table(number):
+	# Initialize the starting point of the multiplication table
+	multiplier = 1
+	# Only want to loop through 5
+	while multiplier <= 5:
+		result = number * multiplier
+		# What is the additional condition to exit out of the loop?
+		if result > 25 :
+			break
+		print(str(number) + "x" + str(multiplier) + "=" + str(result))
+		# Increment the variable for the loop
+		multiplier += 1
+
+multiplication_table(3)
+
+multiplication_table(5)
+
+multiplication_table(8)
